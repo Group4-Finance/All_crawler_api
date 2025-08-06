e70736b24be8a771596325302fdca0767a3dc315
kong <david.5516kimo@gmail.com>
2025-08-07 03:15:37 +0800

    3

--- src/api/main.py
+++ src/api/main.py
@@ -3,9 +3,16 @@ import pandas as pd  # 用來處理資料表
 from fastapi import FastAPI  # 建立 API 用
 from sqlalchemy import create_engine, engine  # 用來建立資料庫連線
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import status
+import numpy as np
+
 
 # 匯入自定義的資料庫連線設定
 from api.config import MYSQL_ACCOUNT, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT
+
+
+
+
 
 
 # 建立連接到 MySQL 資料庫的函式，回傳一個 SQLAlchemy 的連線物件
@@ -16,6 +23,14 @@ def get_mysql_data_conn() -> engine.base.Connection:
     connect = engine.connect()  # 建立實際連線
     return connect  # 回傳連線物件
 
+
+# 建立連接到 MySQL 資料庫的函式，回傳一個 SQLAlchemy 的連線物件
+def get_mysql_data_conn_signal() -> engine.base.Connection:
+    # 組成資料庫連線字串，使用 pymysql 作為 driver
+    address = f"mysql+pymysql://{MYSQL_ACCOUNT}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/ETF_signal"
+    engine = create_engine(address)  # 建立 SQLAlchemy 引擎
+    connect = engine.connect()  # 建立實際連線
+    return connect  # 回傳連線物件
 
 # 建立 FastAPI 應用實例
 app = FastAPI()
@@ -34,13 +49,61 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
 # 定義根目錄路由（測試用）
 @app.get("/")
 def read_root():
     return {"Hello": "World"}  # 回傳基本測試訊息
 
+
+# def nan_to_none(x):
+#     if pd.isna(x):
+#         return None
+#     else:
+#         return x
+
+@app.get("/ETF_signal")
+def get_ETF_signal(
+    Stock_id: str = "",
+    start_date: str = "",
+    end_date: str = "",
+):
+    sql = f"""
+    SELECT * FROM ETF_signal_result_{Stock_id}
+    WHERE Date >= '{start_date}' AND Date <= '{end_date}'
+    """
+    mysql_conn = get_mysql_data_conn_signal()
+    data_df = pd.read_sql(sql, con=mysql_conn)
+
+    data_df["市價"] = data_df["市價"].astype(object)
+    data_df["市價"] = data_df["市價"].where(data_df["市價"].notna(), None)
+    data_df["premium_discount_rate"] = data_df["premium_discount_rate"].astype(object)
+    data_df["premium_discount_rate"] = data_df["premium_discount_rate"].where(data_df["premium_discount_rate"].notna(), None)
+    data_df["折溢價分數"] = data_df["折溢價分數"].astype(object)
+    data_df["折溢價分數"] = data_df["折溢價分數"].where(data_df["折溢價分數"].notna(), None)
+    data_df["VIX"] = data_df["VIX"].astype(object)
+    data_df["VIX"] = data_df["VIX"].where(data_df["VIX"].notna(), None)
+    data_df["指數綜合分數"] = data_df["指數綜合分數"].astype(object)
+    data_df["指數綜合分數"] = data_df["指數綜合分數"].where(data_df["指數綜合分數"].notna(), None)
+    data_df["總分"] = data_df["總分"].astype(object)
+    data_df["總分"] = data_df["總分"].where(data_df["總分"].notna(), None)
+    data_df["燈號"] = data_df["燈號"].astype(object)
+    data_df["燈號"] = data_df["燈號"].where(data_df["燈號"].notna(), None)
+
+
+    print("-----------------------------轉換成None-----------------------------")
+    print(data_df)
+
+    data_dict = data_df.to_dict("records")
+    return {"data": data_dict}
+
+    # mysql_conn = get_mysql_data_conn_signal()
+    # params = {
+    #         "start_date": start_date,
+    #         "end_date": end_date
+    #     }
+    # data_df = pd.read_sql(sql, con=mysql_conn, params= params)
+    # data_dict = data_df.to_dict("records")
+    # return {"data": data_dict}
 
 # 定義取得台灣股價的 API 路由
 @app.get("/cnyes_headlines")
